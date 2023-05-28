3b9d27f1777e3fa0e370bcc5acef25cafd4cecbf
MSQureshi12156 <salmanqureshi5151@gmail.com>
2023-05-28 16:56:47 +0500

    Node grt/post  end-points done

---
+++ .vscode/model/teacherSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+
+const teacherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: Number,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  course: {
+    type: String,
+    required: true
+  },
+  
+});
+
+
+const Teacher = mongoose.model('Teacher', teacherSchema);
+
+
+module.exports = Teacher;
--- controller/route.js
+++ controller/route.js
@@ -1,18 +1,9 @@
 const express = require("express");
-const cors = require("cors");
-const connectDb = require("./Connection/dbconnection");
+const Teacher = require("../.vscode/model/teacherSchema");
 
-const Teacher = require("./.vscode/model/teacherSchema");
+const router = express.Router();
 
-
-const app = express();
-
-connectDb
-app.use(cors());
-app.use(express.json());
-// app.use("/create",router)
-const PORT =5001;
-app.get("/teacher", async(req, res)=>{
+router.get("/teacher", async(req, res)=>{
     try {
         const TeacherData = await Teacher.find({});
         res.status(200).send({message:"Get all data successfully", data:TeacherData})
@@ -21,17 +12,17 @@ app.get("/teacher", async(req, res)=>{
     }
 
 });
-  app.post("/create", async(req, res)=>{
+router.post("/create", async(req, res)=>{
     try {
         const payload = req.body;
         const teacher = new Teacher(payload);
         const TeacherData = await teacher.save();
         res.status(201).send({message:"Data Create successfully", data:TeacherData})
     } catch (error) {
-        res.status(500).send({message:error})
+        res.status(500).send({error})
     }
 
 });
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
+module.exports = router;
